fa00c5f47ed7df2b12af3c4995fcbffb8c89fe67
egebaba31 <egepacal50@gmail.com>
2026-02-04 18:41:05 +0300

    Create ExampleModClient.java

---
+++ src/main/java/com/example/ExampleModClient.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+
+public class ExampleModClient implements ClientModInitializer {
+    public static KeyBinding flyKey;
+    public static KeyBinding killauraKey;
+    public static KeyBinding guiKey;
+
+    public static boolean flyActive = false;
+    public static boolean killauraActive = false;
+
+    @Override
+    public void onInitializeClient() {
+        // Tuş Atamaları
+        flyKey = KeyBindingHelper.registerKeyBinding(new KeyBinding("Fly (Ucus)", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_F, "Hileler"));
+        killauraKey = KeyBindingHelper.registerKeyBinding(new KeyBinding("Killaura", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_R, "Hileler"));
+        guiKey = KeyBindingHelper.registerKeyBinding(new KeyBinding("Hile Menusu", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_K, "Hileler"));
+    }
+}
