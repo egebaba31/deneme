1b7098f597dd5381d63c48c73bc4ff32facab1f2
egebaba31 <egepacal50@gmail.com>
2026-02-04 17:06:55 +0300

    Update ExampleMixin.java

--- src/main/java/com/example/mixin/ExampleMixin.java
+++ src/main/java/com/example/mixin/ExampleMixin.java
@@ -1,15 +1,28 @@
 package com.example.mixin;
 
-import net.minecraft.server.MinecraftServer;
+import com.example.ExampleMod;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MinecraftServer.class)
+@Mixin(ClientPlayerEntity.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadWorld")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadWorld()V
-	}
+    @Inject(at = @At("HEAD"), method = "tick")
+    private void onTick(CallbackInfo info) {
+        ClientPlayerEntity player = (ClientPlayerEntity) (Object) this;
+
+        if (ExampleMod.flyKey.wasPressed()) {
+            ExampleMod.flyActive = !ExampleMod.flyActive;
+            // 1.20.6 sürümünde Text.literal kullanılır
+            player.sendMessage(Text.literal("Fly: " + (ExampleMod.flyActive ? "§aAÇIK" : "§cKAPALI")), true);
+        }
+
+        if (ExampleMod.flyActive) {
+            player.getAbilities().allowFlying = true;
+            player.getAbilities().flying = true;
+        }
+    }
 }
