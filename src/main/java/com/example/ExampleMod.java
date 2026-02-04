a2ecb133043f0f336d99ea9df00b837d42f2e0a0
egebaba31 <egepacal50@gmail.com>
2026-02-04 17:05:48 +0300

    Update ExampleMod.java

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,22 +1,23 @@
 package com.example;
 
 import net.fabricmc.api.ModInitializer;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 
 public class ExampleMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+    public static KeyBinding flyKey;
+    public static boolean flyActive = false;
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
-	}
+    @Override
+    public void onInitialize() {
+        // J tuşu ile Fly aktif olur
+        flyKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "Ucus Modu", 
+            InputUtil.Type.KEYSYM, 
+            GLFW.GLFW_KEY_J, 
+            "Hile Kategorisi"
+        ));
+    }
 }
