fba7c4049134395dddc3efa3532dc26ac10048d0
egebaba31 <egepacal50@gmail.com>
2026-02-04 17:11:21 +0300

    Update ExampleMod.java

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -7,20 +7,18 @@ import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 
 public class ExampleMod implements ModInitializer {
-    // Tuş Atamaları
     public static KeyBinding flyKey;
     public static KeyBinding killauraKey;
     public static KeyBinding guiKey;
 
-    // Hile Durumları
     public static boolean flyActive = false;
     public static boolean killauraActive = false;
 
     @Override
     public void onInitialize() {
-        // J = Uçuş, K = Killaura, M = Menü (GUI)
-        flyKey = KeyBindingHelper.registerKeyBinding(new KeyBinding("Ucus", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_J, "Hileler"));
-        killauraKey = KeyBindingHelper.registerKeyBinding(new KeyBinding("Killaura", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_K, "Hileler"));
-        guiKey = KeyBindingHelper.registerKeyBinding(new KeyBinding("Hile Menusu", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_M, "Hileler"));
+        // Tuşlar senin istediğin gibi: Fly (R), Killaura (F), GUI (K)
+        flyKey = KeyBindingHelper.registerKeyBinding(new KeyBinding("Ucus", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_R, "Hileler"));
+        killauraKey = KeyBindingHelper.registerKeyBinding(new KeyBinding("Killaura", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_F, "Hileler"));
+        guiKey = KeyBindingHelper.registerKeyBinding(new KeyBinding("Hile Menusu", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_K, "Hileler"));
     }
 }
